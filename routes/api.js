9bc99c23a487d33feddb8df0a851174932183970
zeb <zeb.zhao@gamecheetah.net>
2016-04-20 02:57:56 -0400

    found better way than running phantom js on server

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var phantom = require('phantom');
+var crypto = require('crypto');
+var md5sum = crypto.createHash('md5');
+var fs = fs = require('fs');
 
-var sitepage = null;
+var sitePage = null;
 var phInstance = null;
+var tasks = [];
 
 phantom.create()
     .then(function(instance) {
@@ -11,13 +15,13 @@ phantom.create()
         return phInstance.createPage();
     })
     .then(function(page) {
-        sitepage = page;
-        return page.open('http://localhost:3000');
+        sitePage = page;
+        return sitePage.open('http://localhost:3000');
     })
     .then(function(status) {
         if (status == 'fail') {
             console.log(status);
-            sitepage.close();
+            sitePage.close();
             phInstance.exit();
         }
     })
@@ -25,6 +29,19 @@ phantom.create()
         console.log(error);
         phInstance.exit();
     });
+
+router.get('/tasks', function(req, res, next) {
+    res.send(tasks);
+});
+
+router.post('/tasks', function(req, res, next) {
+    var url = req.body.url;
+    var hash = md5sum.update(name).digest('hex');
+    if (hash) {
+        tasks.push({url: url});
+        res.status(204).send();
+    }
+});
 
 router.get('/urls', function(req, res, next) {
 
